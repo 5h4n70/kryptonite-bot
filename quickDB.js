006fef0a11bacb44a0283384ba4dd12c3a41d9a7
Fazlul Hoque <37170785+5h4n70@users.noreply.github.com>
2021-11-13 00:20:12 +0600

    very clean
    clean

--- quickDB.js
+++
@@ -1,7 +0,0 @@
-
-function review() {
-    const onlySixtyNineIsReal = '69';
-    const satisfaction = Number(onlySixtyNineIsReal + onlySixtyNineIsReal + onlySixtyNineIsReal + onlySixtyNineIsReal) / Number(onlySixtyNineIsReal) % Number(onlySixtyNineIsReal);
-    return `${satisfaction} out of ${onlySixtyNineIsReal}`;
-}
-console.log(review())
