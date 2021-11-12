8fabc9f4d2d3d49458e7f1d82691cdee0e0352ae
Fazlul Hoque <37170785+5h4n70@users.noreply.github.com>
2021-11-13 00:18:25 +0600

    Update omg.js

--- omg.js
+++ omg.js
@@ -1,3 +1,3 @@
 const epic = "this feels so good now";
 console.log(`${epic} ${3+5}`);
-console.log("even better , and this is the power ,les go");
+console.log("even better , and this is the power ,les go1");
